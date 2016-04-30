fb04d0fd83ec73b5a6c06a282392836ac807d013
Darshan Manjrekar <darshanmanjrekar1205@gmail.com>
2016-04-30 21:31:46 +0530

    Add Flashing Tools for Linux Users
    Tool has been tested by flashing YU YUREKA PLUS with "cm-12.1-YOG4PAS3JM-tomato-signed-fastboot-package" 
    Log in terminal: http://pastebin.com/wnQEppxG

---
+++ flash-all.sh
@@ -0,0 +1,25 @@
+echo ---------------------[ Start flashing ROM ]-------------------------
+echo ----------------[ Checking for connected devices ]------------------
+fastboot -i 0x1ebf devices 
+echo -------------------[ Unlocking bootloader ]-------------------------
+fastboot -i 0x1ebf oem unlock 
+echo ---------------[ Flashing modem and bootloader ]--------------------
+fastboot -i 0x1ebf flash aboot emmc_appsboot.mbn
+fastboot -i 0x1ebf flash modem NON-HLOS.bin
+fastboot -i 0x1ebf flash rpm rpm.mbn
+fastboot -i 0x1ebf flash sbl1 sbl1.mbn
+fastboot -i 0x1ebf flash tz tz.mbn
+fastboot -i 0x1ebf flash hyp hyp.mbn
+echo ----------------------[ Flashing kernel ]---------------------------
+fastboot -i 0x1ebf flash boot boot.img
+echo ----------------------[ Flashing recovery ]-------------------------
+fastboot -i 0x1ebf flash recovery recovery.img
+echo -------------------[ Flashing system partition ]--------------------
+fastboot -i 0x1ebf flash system system.img
+echo -------------------[ Flashing data partition ]----------------------
+fastboot -i 0x1ebf flash userdata userdata.img
+echo ---------------------[ Relocking bootloader ]-----------------------
+fastboot -i 0x1ebf oem lock
+echo ---------------------[ Rebooting device ]---------------------------
+fastboot -i 0x1ebf reboot
+echo -------------------------[ ALL DONE ]-------------------------------
