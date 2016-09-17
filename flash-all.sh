e1e0ec587cd758d189ff52733caffd0caf2fbe8b
AudioGod <adarsh.mmx@micromaxinfo.com>
2016-09-17 15:11:26 +0530

    Add Commands to Flash modem and bootloader
    Signed-off-by: AudioGod <audiogod@sungsonic.co>

--- flash-all.sh
+++ flash-all.sh
@@ -7,6 +7,14 @@ echo -------------------[ Unlocking bootloader ]-------------------------
 fastboot oem unlock
 echo ----------------------[ Flashing kernel ]---------------------------
 fastboot flash boot boot.img
+echo ---------------[ Flashing modem and bootloader ]--------------------
+fastboot flash aboot emmc_appsboot.mbn
+fastboot flash modem NON-HLOS.bin
+fastboot flash rpm rpm.mbn
+fastboot flash sbl1 sbl1.mbn
+fastboot flash tz tz.mbn
+fastboot flash hyp hyp.mbn
+fastboot flash splash splash.img
 echo ----------------------[ Flashing recovery ]-------------------------
 fastboot flash recovery recovery.img
 echo -------------------[ Flashing system partition ]--------------------
